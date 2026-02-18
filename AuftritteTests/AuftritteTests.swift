b42c27b667e249cf0175c66f18861d88ef698386
Thomas Suessli <thommy.suessli@bluewin.ch>
2026-02-18 12:15:51 +0900

    Initial Commit

---
+++ AuftritteTests/AuftritteTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuftritteTests.swift
+//  AuftritteTests
+//
+//  Created by Thomas Süssli on 18.02.2026.
+//
+
+import Testing
+
+struct AuftritteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
